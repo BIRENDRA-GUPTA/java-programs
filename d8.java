ceb965c0aec93bb8853aa88aa0d69076120fcd31
BIRENDRA-GUPTA <102951925+BIRENDRA-GUPTA@users.noreply.github.com>
2023-03-30 23:56:19 +0530

    Add files via upload

---
+++ d8.java
@@ -0,0 +1,11 @@
+public class d8 {
+    public static void main(String[] args) {
+        String word = "BIRENDRA KUMAR GUPTA";
+		
+		for(int i = 0; i < word.length(); i+= 2){
+			
+			System.out.println(word.charAt(i) + "" + word.charAt(i + 1));
+			
+		}
+    }
+}
